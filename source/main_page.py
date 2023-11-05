db35ef210dae173638a56736b11d507de91417ac
Laura Guerra <laugueriv@gmail.com>
2023-11-05 19:37:43 +0100

    Add url functions

--- source/main_page.py
+++ source/main_page.py
@@ -10,14 +10,40 @@ quadrants = [(41.44499 ,  2.16561, 41.41053333333327, 2.12304),
              (41.38867666666667, 2.165615, 41.36052, 2.12304),
              (41.38867666666667, 2.12304, 41.36052, 2.165615)]
 
+dates = [('2023-11-13', '2023-11-15'),
+         ('2023-11-17', '2023-11-19'),
+         ('2023-12-08', '2023-12-10')]
 
-def build_urls(url, listings_per_page=20, pages_per_location=15):
+
+
+def build_urls(url, listings_per_page=18, pages_per_location=15):
     """Builds links for all search pages for a given location"""
     
     url_list = []
+
     for i in range(pages_per_location):
         offset = listings_per_page * i
         url_pagination = url + f'&items_offset={offset}'
+
+        for quadrant in quadrants:
+            url_def = url_pagination + add_quadrants(quadrant)
+            
+            for date in dates:
+                url_def = url_def + add_date(date)
+        
         url_list.append(url_pagination)
+
        
     return url_list
+
+
+def add_quadrants(quadrant):
+     
+     parameter = '&nelat=' + quadrant[0] + '&nelong=' + quadrant[1] + '&swlat=' + quadrant[2] + 'swlong' + quadrant[3]
+     return parameter
+
+
+def add_date(dates):
+    parameter = '&checkin=' + dates[0] + '&checkout=' + dates[1]
+    return parameter
+
